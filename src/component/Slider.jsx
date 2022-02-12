52815b007b77882dc11f8f404263009cf110d9aa
SShak <shawnshaknitz@gmail.com>
2022-02-12 15:55:32 -0500

    slider finished

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,12 +1,15 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import { useState } from 'react';
 import styled from 'styled-components';
+import { sliderItems } from '../data';
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-   // background-color: coral;
+    //background-color: coral;
     position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -25,42 +28,89 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${props=>props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${props=>props.bg};
 `
 const ImgContainer = styled.div`
+    height: 100%;
     flex: 1;
+    //margin-left: 250px;
 `
 
 const Image = styled.img`
-    
+    height: 80%;
+    padding-left: 250px;
 `
 
 const InfoContainer = styled.div`
     flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Description = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 
 export const Slider = () => {
+        const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+        
+      if(direction==="left"){
+          setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2 )
+      }  else {
+          setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0 )
+      }
+    };
+
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlined />
         </Arrow>
-        <Wrapper>
-            <ImgContainer>
-                <Image src='https://craft-mart.com/wp-content/uploads/2017/10/acrylic-pouring-featured-1.jpg'/>
-            </ImgContainer>
-            <InfoContainer></InfoContainer>
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((item) => (
+            <Slide bg={item.bg}>
+                <ImgContainer  ImgContainer>
+                    <Image src={item.img} />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>{item.title}</Title>
+                    <Description>{item.desc}</Description>
+                    <Button>Transend</Button>
+                </InfoContainer>
+            </Slide>
+            ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlined />
         </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const sliderItems = [
+
+    {
+        id: 1,
+        img: "https://www.nicepng.com/png/full/102-1028484_but-we-only-need-design-all-seeing-eye.png",
+        title: "The light within is you",
+        desc: "Follow me to know all",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://www.chakras.info/wp-content/uploads/open-third-eye.jpg",
+        title: "See all",
+        desc: "Be all",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/71XMPsbXBNL._SL1333_.jpg",
+        title: "Open your mind",
+        desc: "Go beyond",
+        bg: "f5fafd",
+    },
+
+
+
+]
